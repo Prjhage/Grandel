c35c4d236a019315af67a7d3316d4395643cc50f
Prajwal Hage <prajwal.hage23@pccoepune.org>
2026-02-02 00:44:24 +0530

    Fix backend routes and login form

--- Backend/app.js
+++ Backend/app.js
@@ -25,7 +25,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle Preflight Requests explicitly
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
